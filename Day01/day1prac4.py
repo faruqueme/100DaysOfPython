a69c20269d5e2b277000bc526ab46a9934eeef35
faruqueme <faruquebinkhaleque@gmail.com>
2022-11-07 20:24:42 +0600

    refacter :hammer:

--- Day01/day1prac4.py
+++ Day01/day1prac4.py
@@ -1,4 +1,4 @@
-#input() is a function that takes input prompt
-#print() function helps to show the result that you input in the result of input() function
+# input() is a function that takes input prompt
+# print() function helps to show the result that you input in the result of input() function
 
 print("Hello" + input("what's your name?"))
